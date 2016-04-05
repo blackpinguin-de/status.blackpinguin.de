93631d457c68c4e313ffea96f1b549f1ae5abe6d
R. C. Ladiges <rcl@blackpinguin.de>
2016-04-05 21:30:37 +0200

    color upgrade and backup dates red if they're older than one week

--- php/main.php
+++ php/main.php
@@ -9,9 +9,16 @@ require_once('disks.php');
 
 $runtime = trim(@shell_exec("bash/uptime.py"));
 $distro = trim(@shell_exec("cat /etc/os-release | grep 'PRETTY_NAME=\"' | sed -re 's|[^\"]+=\"([^\"]+)\"|\\1|g'"));
-$apt = trim(@shell_exec('sec=$(stat -c %Y /var/cache/apt/) ; date -d "@$sec" +"%Y-%m-%d %H:%M:%S %Z"'));
 $runlevel = trim(@shell_exec("who -r | grep -Po 'run-level \\d ' | grep -Po '\\d'"));
 $boottime = trim(@shell_exec("x=$(LANG='en_US.utf8'; who -b | grep -Po 'boot .*' | grep -Po ' .*' | grep -Po '20\\d\\d-\\d\\d-\\d\\d \\d\d:\\d\\d.*') ; date -d \"\$x GMT+2\" +'%F %T %Z'"));
+
+$apt = trim(@shell_exec('sec=$(stat -c %Y /var/cache/apt/) ; date -d "@$sec" +"%Y-%m-%d %H:%M:%S %Z"'));
 $last_backup = trim(@shell_exec("cat /rcl/logs/last_backup.txt"));
+$now   = time();
+$aptt  = strtotime('+1 week', strtotime($apt));
+$backt = strtotime('+1 week', strtotime($last_backup));
+$apt = "<span class='".($aptt  < $now ? 'red' : 'green')."'>$apt</span>";
+$last_backup = "<span class='".($backt < $now ? 'red' : 'green')."'>$last_backup</span>";
+
 
 ?>
